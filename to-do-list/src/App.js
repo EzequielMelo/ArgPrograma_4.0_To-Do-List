2d22bf10c747bdf2b4491a574814e24beaa1b0b9
EzequielMelo <ezequielanthonymelo@gmail.com>
2023-11-29 17:55:04 -0300

    se añadio al proyecto Bootstrap como herramienta

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div >
+    <div>
       <Home/>
     </div>
   );
--- to-do-list/src/components/Home.jsx
+++ to-do-list/src/components/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Home() {
   return (
-    <div>
-        Home
-    </div>
+    <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home">To-Do List</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 

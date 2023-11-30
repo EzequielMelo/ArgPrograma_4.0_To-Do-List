7e8e822b47f6fccd11847df118135942e5ff3db0
EzequielMelo <ezequielanthonymelo@gmail.com>
2023-11-30 03:27:59 -0300

    se elimino bootstrap del proyecto, finalmente se haran las cosas a mano

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import Home from './components/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Task from './components/Task';
+
 
 function App() {
   return (
     <div>
       <Home/>
-      <Task/>
     </div>
   );
 }
